c7f310502e44f84ac1f034b85b5b768c8b67417d
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-09-12 09:00:22 +0200

    create new folders!!

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import '../public/css/App.css'
 import HeaderComp from './components/header'
-import SectionHead from './section-head'
-import SectionDisp from './section-disponible'
+import SectionHead from './pages/head/section-head'
+import SectionDisp from './pages/disp/section-disponible'
 import Footer from './components/footer'
 import Terms from './components/termsCond'
-import Calendar from './section-Calendar'
 
 function App() {
   const [count, setCount] = useState(0)
